8a5fff34dffa24ca1f9d9ca159f26ea43726bfa8
Darin Krauss <darin@tidepool.org>
2021-03-03 22:31:12 -0800

    [LOOP-2546] Display onboarding at startup
    - https://tidepool.atlassian.net/browse/LOOP-2546
    - https://tidepool.atlassian.net/browse/LOOP-3143
    - https://tidepool.atlassian.net/browse/LOOP-3144
    - https://tidepool.atlassian.net/browse/LOOP-3145
    - Implement OnboardingUI protocol updates
    - Fix Suspend Threshold Info view sometimes displaying twice

--- LoopOnboardingKitUI/LoopOnboardingUI.swift
+++ LoopOnboardingKitUI/LoopOnboardingUI.swift
@@ -13,13 +13,55 @@ import LoopKitUI
 import LoopOnboardingKit
 
 public final class LoopOnboardingUI: OnboardingUI {
-    public let onboardingIdentifier = "LoopOnboarding"
-
     public static func createOnboarding() -> OnboardingUI {
-        return LoopOnboardingUI()
+        return Self()
     }
 
-    public func onboardingViewController(cgmManagerProvider: CGMManagerProvider, pumpManagerProvider: PumpManagerProvider, serviceProvider: ServiceProvider, preferredGlucoseUnit: HKUnit, colorPalette: LoopUIColorPalette) -> (UIViewController & OnboardingViewController) {
-        return OnboardingUICoordinator(initialTherapySettings: TherapySettings(), cgmManagerProvider: cgmManagerProvider, pumpManagerProvider: pumpManagerProvider, serviceProvider: serviceProvider, preferredGlucoseUnit: preferredGlucoseUnit, colorPalette: colorPalette)
+    public weak var onboardingDelegate: OnboardingDelegate?
+
+    public let onboardingIdentifier = "LoopOnboarding"
+
+    public var isOnboarded: Bool {
+        didSet {
+            guard isOnboarded != oldValue else { return }
+            notifyDidUpdateState()
+        }
+    }
+
+    var therapySettings: TherapySettings? {
+        didSet {
+            guard therapySettings != oldValue, let therapySettings = therapySettings else { return }
+            notifyHasNewTherapySettings(therapySettings)
+        }
+    }
+
+    init() {
+        self.isOnboarded = false
+    }
+
+    public init?(rawState: RawState) {
+        guard let isOnboarded = rawState["isOnboarded"] as? Bool else {
+            return nil
+        }
+
+        self.isOnboarded = isOnboarded
+    }
+
+    public var rawState: RawState {
+        return [
+            "isOnboarded": isOnboarded
+        ]
+    }
+
+    public func onboardingViewController(onboardingProvider: OnboardingProvider, preferredGlucoseUnit: HKUnit, colorPalette: LoopUIColorPalette) -> (UIViewController & OnboardingViewController) {
+        return OnboardingUICoordinator(onboarding: self, onboardingProvider: onboardingProvider, initialTherapySettings: TherapySettings(), preferredGlucoseUnit: preferredGlucoseUnit, colorPalette: colorPalette)
+    }
+
+    private func notifyDidUpdateState() {
+        onboardingDelegate?.onboardingDidUpdateState(self)
+    }
+
+    private func notifyHasNewTherapySettings(_ therapySettings: TherapySettings) {
+        onboardingDelegate?.onboarding(self, hasNewTherapySettings: therapySettings)
     }
 }
